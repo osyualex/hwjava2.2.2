72b8e50e2660321a0b4202a6c846b937746dbaf8
Aleksandr Osyukov <ocukov@mail.ru>
2023-03-05 12:59:48 +0300

    second commit

--- bmi/src/BmiService.java
+++ bmi/src/BmiService.java
@@ -1,31 +1,7 @@
 public class BmiService {
-    public int calculate(int a, int b) {
-        int c = b * b ;
-        int f = a * 10000 ;
-        int imb = f / c ;
-        if ( 16 > imb ) {
-            System.out.println(" Выраженный дефицит массы тела");
-        }
-        else if (19 >= imb && 16 < imb){
-            System.out.println(" Недостаточная (дефицит) масса тела");
-        } 
-        else if (25 >= imb && 19 < imb ) {
-            System.out.println(" Норма");
-        }
-        else if ( 30 >= imb && 25 < imb) {
-            System.out.println(" Избыточная масса тела (предожирение)");
-        }
-        else if ( 35 >= imb && 30 < imb) {
-            System.out.println(" Ожирение первой степени");
-        }
-        else if ( 40 >= imb && 35 < imb) {
-            System.out.println(" Ожирение второй степени");
-        }
-        else if ( 40 <= imb) {
-            System.out.println( imb + " Ожирение третьей степени (морбидное)");
-        }
-        return imb;
+    public int calculate(int weight, int growth) {
+        int bmi = (int) (weight * 10000 / Math.pow(growth, 2));
 
-
+        return bmi;
     }
 }
